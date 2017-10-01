45b420312e68ea07a550a6e4daa02e2dfdfc75de
raindropgd <32434840+raindropgd@users.noreply.github.com>
2017-10-01 23:35:16 +0300

    Add files via upload

--- get_started.js
+++ get_started.js
@@ -6,12 +6,15 @@ $(function () {
 		setTimeout(function () {
 			$("#h1").addClass('title_1_in_vision');
 			$("#h2").addClass('title_2_in_vision');
+			$(".rains_container").addClass('rains_container_start_rain');
+
 		},1000);
 
 		setTimeout(function () {
 			$(".get_started_btn").addClass('get_started_in_vision');
 			
 		},1500);
+
 	
 	});
 });
