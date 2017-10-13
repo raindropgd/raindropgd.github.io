ae19c6e678df146bdd2ce78216c179d74726ccdf
raindropgd <32434840+raindropgd@users.noreply.github.com>
2017-10-13 12:48:39 +0300

    Add files via upload

---
+++ jobs.js
@@ -0,0 +1,23 @@
+$(function () {
+	
+	jQuery(document).ready(function($) {
+		$(".hamburger_nav").click(function(event) {
+			$(".menu_burger_containter").slideToggle();
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	});//rdy
+});//main
